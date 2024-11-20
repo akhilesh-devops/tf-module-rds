f0c0f96ef266a60e060ef451d287f8b5b5110a34
AKhilesh <kothapallyakhil123@gmail.com>
2024-11-20 13:49:23 +0530

    test

---
+++ variable.tf
@@ -0,0 +1,14 @@
+variable "tags" {}
+variable "env" {}
+variable "rds_type" {}
+variable "subnet_ids" {}
+variable "sg_ingress_cidr" {}
+variable "db_port" {}
+variable "vpc_id" {}
+variable "engine_family" {}
+variable "engine" {}
+variable "engine_version" {}
+variable "backup_retention_period" {}
+variable "preferred_backup_window" {}
+variable "instance_class" {}
+variable "instance_count" {}
